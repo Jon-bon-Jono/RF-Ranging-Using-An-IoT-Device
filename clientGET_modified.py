581227ee313c2ae25077f178e476a9e1ee631806
Jon-bon-Jono <36943788+Jon-bon-Jono@users.noreply.github.com>
2020-12-20 18:34:58 +1100

    Update clientGET_modified.py

--- clientGET_modified.py
+++ clientGET_modified.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-# This file is part of the Python aiocoap library project.
-#
-# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
-#               2013-2014 Christian <c.amsuess@energyharvesting.at>
-#
-# aiocoap is free software, this file is published under the MIT license as
-# described in the accompanying LICENSE file.
-
-#by Jonathan Williams (z5162987) and Cavinash Chelliah (z3417347)
+#by Jonathan Williams
 #User promted to Enter distance in cm
 #Sends 10 requests to CoAP server
 #Receives 10 rssi values
